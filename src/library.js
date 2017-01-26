5893ff3a0af731e51615ff6bbbb867dcf9c27987
Riki Fridrich <riki@fczbkk.com>
2017-01-26 14:10:31 +0100

    chore: add documentation

--- src/library.js
+++ src/library.js
@@ -1,11 +1,29 @@
+/**
+ * Sum two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export function add (a, b) {
   return a + b;
 }
 
+/**
+ * Subtract two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export function subtract (a, b) {
   return a - b;
 }
 
+/**
+ * Multiply two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
 export function multiply (a, b) {
   return a * b;
 }
