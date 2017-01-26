9141f4f475a02c3c2c01f914de5f09702e275dd3
Riki Fridrich <riki@fczbkk.com>
2017-01-26 13:54:19 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {add} from './library.js';
+
+console.log('add', add(1, 2));
+
+// console.log(`subtract ${1 - 2}`, subtract(1, 2));
---
+++ webpack.conf.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './build/',
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    })
+  ]
+};
